8f24e5e3771e9a6559e49bf13f1992e83be8b5ef
jake <jclark208@yahoo.com>
2020-07-08 12:57:11 -0400

    all completed still working on randomly adding items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,23 @@
-from room import Room
+from room import Rooms
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Rooms("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Rooms("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Rooms("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Rooms("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Rooms("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -49,3 +50,71 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+player = Player("jake","outside")
+
+is_playing = True
+
+# while is_playing:
+#     print(jake.room)
+#     print(room[jake.room].description)
+#     direction = input("Which way? n,e,s,w")
+#     if direction == "n":
+#         jake.room = room[jake.room].n_to
+#     print(jake.room)
+#     is_playing = False
+
+while is_playing:
+    print(room[player.room])
+    # TODO: (the textwrap module might be useful here).
+    direction = input('Where do you want to go? N, E, S, W? To pickup or drop items use the command take "item" or drop "item"').lower().split()
+    if direction[0] == 'n':
+        if player.room == 'outside':
+            player.room = 'foyer'
+            print(player.room)
+        elif player.room == 'foyer':
+            player.room ='overlook'
+        elif player.room == 'narrow':
+            player.room = 'treasure'
+        else:
+            direction = input('Please choose another direction: S, W, E ')
+    elif direction[0] == 'e':
+        if player.room == 'foyer':
+            player.room = 'narrow'
+        else:
+            direction = input('Please choose another direction: N, S, W ')
+    elif direction[0] == 's':
+        if player.room == 'foyer':
+            player.room = 'outside'
+        elif player.room == 'overlook':
+            player.room = 'foyer'
+        elif player.room == 'treasure':
+            player.room = 'narrow'
+        else:
+            direction = input('Please choose another direction: N, W, E ')
+    elif direction[0] == 'w':
+        if player.room == 'narrow':
+            player.room = 'foyer'
+        else:
+            direction = input('Please choose another direction: N, S, E ')
+    elif direction[0] == 'q':
+        is_playing = False
+    elif direction[0] == 'take' or direction[0] == 'get':
+        if direction[1] in room[player.room].loot:
+            player.add_item(direction[1])
+            room[player.room].remove_item(direction[1])
+            print(f'You picked up: {direction[1]}')
+        else: direction =input('Well clearly you are an idiot! Do you want to try that again?')
+    elif direction[0] == 'drop' or direction[0] == 'remove':
+        if direction[1] in room[player.room].loot:
+            player.drop_item(direction[1])
+            room[player.room].add_item(direction[1])
+            print(f'You dropped: {direction[1]}')
+        else: direction =input('Well clearly you are an idiot! Do you want to try that again?')
+    elif direction[0] == 'yeet':
+         if direction[1] in room[player.room].loot:
+            player.drop_item(direction[1])
+            room[player.room].add_item(direction[1])
+            print(f'You yeeted: {direction[1]} into another dimension! Continue on baller!')
+    else:
+        direction = input('Please choose a valid direction, or press q to quit')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Rooms:
+    def __init__(self,name,description,n_to=None,s_to=None,e_to=None,w_to=None):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+        self.loot = []
+    def __str__(self):
+        return f'This is the {self.name}. Description: {self.description} Holy crap there are a few items in here: {self.loot}'
+        
+    def remove_item(self, item):
+        self.loot.remove(item)
+
+    def add_item(self, item):
+        self.loot.append(item)
